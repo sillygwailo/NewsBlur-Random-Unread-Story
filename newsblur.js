98bde13285b107bc67774e7717165cb507a62ad1
Richard Eriksson <richard@justagwailo.com>
2014-03-13 23:54:26 -0700

    Initial commit.

---
+++ newsblur.js
@@ -0,0 +1,45 @@
+var r = require('request').defaults({jar: true});
+
+var options = require(__dirname + '/newsblur_options.js');
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+var login_options = {
+  url: 'https://www.newsblur.com/api/login',
+  headers: {
+    'User-Agent': 'NewsBlur Random Unread Story',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: "username=" + options.username + '&password=' + options.password
+}
+r.post(login_options, function login(error, response, body) {
+  var hashes_options = {
+    url: 'https://www.newsblur.com/reader/unread_story_hashes',
+    headers: {
+      'User-Agent': 'NewsBlur Random Unread Story',
+    }
+  };
+  r.get(hashes_options, function feeds(error, response, body) {
+    var unread_feed_story_hashes_obj = JSON.parse(body);
+    var feed_ids = Object.keys(unread_feed_story_hashes_obj.unread_feed_story_hashes);
+    var random_feed = getRandomInt(0, feed_ids.length);
+    for (var feed_id_key in feed_ids) {
+      if (feed_id_key == random_feed) {
+        feed_id = feed_ids[random_feed];
+        var feed_options = {
+          url: 'https://www.newsblur.com/reader/feed/' + feed_id,
+          headers: {
+            'User-Agent': 'NewsBlur Random Unread Story',
+          }
+        }
+        r.get(feed_options, function story(error, response, body) {
+          stories_obj = JSON.parse(body);
+          stories = stories_obj.stories;
+          story_id = getRandomInt(0, stories.length);
+          console.log(stories[story_id].story_permalink);
+        });
+      }
+    } // for in
+  });
+});
---
+++ newsblur_options.js
@@ -0,0 +1,4 @@
+module.exports = {
+  username: 'YOUR_EMAIL_ADDRSSS',
+  password: 'YOUR_PASSWORD',
+}
