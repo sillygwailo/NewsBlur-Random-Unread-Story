36b164ebe438df2885cc20f9b8ae0a234c8a76e7
Richard Eriksson <richard@justagwailo.com>
2014-03-14 11:57:51 -0700

    Fix for undefined story permalink.

--- newsblur.js
+++ newsblur.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var r = require('request').defaults({jar: true});
 
 var options = require(__dirname + '/newsblur_options.js');
@@ -23,7 +24,7 @@ r.post(login_options, function login(error, response, body) {
   r.get(hashes_options, function feeds(error, response, body) {
     var unread_feed_story_hashes_obj = JSON.parse(body);
     var feed_ids = Object.keys(unread_feed_story_hashes_obj.unread_feed_story_hashes);
-    var random_feed = getRandomInt(0, feed_ids.length);
+    var random_feed = getRandomInt(0, feed_ids.length - 1);
     for (var feed_id_key in feed_ids) {
       if (feed_id_key == random_feed) {
         feed_id = feed_ids[random_feed];
@@ -36,7 +37,7 @@ r.post(login_options, function login(error, response, body) {
         r.get(feed_options, function story(error, response, body) {
           stories_obj = JSON.parse(body);
           stories = stories_obj.stories;
-          story_id = getRandomInt(0, stories.length);
+          story_id = getRandomInt(0, stories.length - 1);
           console.log(stories[story_id].story_permalink);
         });
       }
